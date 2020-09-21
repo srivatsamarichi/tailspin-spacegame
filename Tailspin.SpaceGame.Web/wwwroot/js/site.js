07531a0fc2d28c73f311553ada2289eb517dbed6
SrivatsaMarichi <srivatsams@yahoo.com>
2020-09-21 21:59:31 +0530

    adding ESLint errors

--- Tailspin.SpaceGame.Web/wwwroot/js/site.js
+++ Tailspin.SpaceGame.Web/wwwroot/js/site.js
@@ -3,3 +3,18 @@
  ScrollSpy.prototype.clear = function () {
     $(this.selector)
 // Write your JavaScript code.
+// {a} is already defined
+
+function test() {
+    "use strict";
+    var a = 1,
+        a = 2;
+    return a;
+}
+
+// Avoid arguments.{a}
+var numbers = [1, 2, 3, 4, 5];
+numbers.map(function (n) {
+    return n < 1 ? 1 : n * arguments.callee(n - 1);
+});
+// Returns [1, 2, 6, 24, 120]
