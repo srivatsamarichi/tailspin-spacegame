33d5f4259a1f864673ef9d7ae286fe4c3fd52fc8
SrivatsaMarichi <srivatsams@yahoo.com>
2020-09-21 22:11:01 +0530

    updated js

--- Tailspin.SpaceGame.Web/wwwroot/js/site.js
+++ Tailspin.SpaceGame.Web/wwwroot/js/site.js
@@ -11,10 +11,3 @@ function test() {
         a = 2;
     return a;
 }
-
-// Avoid arguments.{a}
-var numbers = [1, 2, 3, 4, 5];
-numbers.map(function (n) {
-    return n < 1 ? 1 : n * arguments.callee(n - 1);
-});
-// Returns [1, 2, 6, 24, 120]
