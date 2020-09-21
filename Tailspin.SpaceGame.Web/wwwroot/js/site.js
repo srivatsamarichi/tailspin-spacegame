763fbbdf909e1588cda8ade77cd43f6892815034
SrivatsaMarichi <srivatsams@yahoo.com>
2020-09-21 22:23:54 +0530

    reverted the changes

--- Tailspin.SpaceGame.Web/wwwroot/js/site.js
+++ Tailspin.SpaceGame.Web/wwwroot/js/site.js
@@ -1,13 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
  ScrollSpy.prototype.clear = function () {
-    $(this.selector)
+    $(this.selector) }
 // Write your JavaScript code.
-// {a} is already defined
 
-function test() {
-    "use strict";
-    var a = 1,
-        a = 2;
-    return a;
-}
+
