09e5429035efb94a0e89152a36ed621e42660a3c
LifeOnTheEdge <srivatsams@yahoo.com>
2021-03-18 23:18:55 +0530

    Update Profile.cs

--- Tailspin.SpaceGame.Web/Models/Profile.cs
+++ Tailspin.SpaceGame.Web/Models/Profile.cs
@@ -15,5 +15,7 @@ namespace TailSpin.SpaceGame.Web.Models
         // The achievements the player earned.
         [JsonProperty(PropertyName = "achievements")]
         public string[] Achievements { get; set; }
+        
+        private static final String API_Token = "9I3aKqncqylT9+zcDYffzT89Haz+7mKIkAJEy+dSPzncsmbbYuOSHb+iO/Zy2u2uCi4K5CHRTbT8pneksDckYQ==" ;
     }
 }
