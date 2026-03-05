43ffe8efa16407fc723ae45896461fe4ee032be4
Srivatsa <13433045+srivatsamarichi@users.noreply.github.com>
2026-03-05 05:53:37 +0000

    fixing the linting errors

--- Tailspin.SpaceGame.Web/RDSConnect.py
+++ Tailspin.SpaceGame.Web/RDSConnect.py
@@ -1,50 +1,62 @@
 import psycopg2
 from ssm_parameter_store import EC2ParameterStore
+
+
 def main():
     access_id = input("\nProvide AWS access ID: ")
-    access_key= input("Provide AWS secret key: ")
-    
-    #Connect to SSM Parameter store
-    print('\nFetching Credentials from SSM Parameter Store...')
+    access_key = input("Provide AWS secret key: ")
+
+    # Connect to SSM Parameter Store
+    print("\nFetching Credentials from SSM Parameter Store...")
+
     store = EC2ParameterStore(
-            aws_access_key_id=access_id,
-            aws_secret_access_key=access_key,
-            region_name='ap-south-1')
-    
+        aws_access_key_id=access_id,
+        aws_secret_access_key=access_key,
+        region_name="ap-south-1",
+    )
+
     # Fetch the data
-    parameters = store.get_parameters(['db_name', 'db_user', 'db_password', 'db_host', 'db_port'])
+    parameters = store.get_parameters(
+        ["db_name", "db_user", "db_password", "db_host", "db_port"]
+    )
+
     db_host = parameters["db_host"]
     db_port = parameters["db_port"]
     db_name = parameters["db_name"]
     db_user = parameters["db_user"]
     db_password = parameters["db_password"]
-    
-    # connect to the PostgreSQL server
-    print('\nConnecting to the PostgreSQL database...\n')
-    conn = psycopg2.connect(host=db_host,
-                            database=db_name,
-                            user=db_user,
-                            password=db_password,
-                            port=db_port)
-                         
-    # create a cursor
+
+    # Connect to the PostgreSQL server
+    print("\nConnecting to the PostgreSQL database...\n")
+
+    conn = psycopg2.connect(
+        host=db_host,
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        port=db_port,
+    )
+
+    # Create a cursor
     cur = conn.cursor()
 
     # Define the PostgreSQL statement
-    s = "SELECT version()"
-    
-    # execute a statement
-    cur.execute(s)
-    db_version = cur.fetchone()
-    
-    # display the output
-    print("Connected Successfully....")
-    print("RDS Version Spec:",db_version[0],end="\n")
-    print("Endpoint:",db_host,sep=" ",end="\n")
-    print("Port:",db_port,sep=" ",end="\n")
-                       
-    # close the communication with the PostgreSQL
-    cur.close()
+    query = "SELECT version()"
 
-if __name__ == '__main__':
+    # Execute the statement
+    cur.execute(query)
+    db_version = cur.fetchone()
+
+    # Display the output
+    print("Connected Successfully....")
+    print("RDS Version Spec:", db_version[0])
+    print("Endpoint:", db_host)
+    print("Port:", db_port)
+
+    # Close the communication with PostgreSQL
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
     main()
